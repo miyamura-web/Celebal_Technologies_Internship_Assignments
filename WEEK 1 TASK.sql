0a7d6bd5c830639ec4c3b5cedd9de2f04b6570a3
NILADRI HALDER <niladrih03@gmail.com>
2025-06-05 13:05:43 +0530

    Add files via upload

---
+++ WEEK 1 TASK.sql
@@ -0,0 +1,136 @@
+
+
+-- Q1:
+
+SELECT 
+    c.CustomerID,
+    p.FirstName,
+    p.LastName,
+    s.Name AS StoreName,
+    c.TerritoryID
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID;
+
+-- Q2 :
+
+SELECT 
+    c.CustomerID,
+    s.Name AS CompanyName
+FROM Sales.Customer c
+JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+WHERE s.Name LIKE '%N';
+
+
+-- Q3 :
+SELECT 
+    c.CustomerID,
+    p.FirstName,
+    p.LastName,
+    a.City
+FROM Sales.Customer c
+JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+JOIN Person.BusinessEntityAddress bea ON p.BusinessEntityID = bea.BusinessEntityID
+JOIN Person.Address a ON bea.AddressID = a.AddressID
+WHERE a.City IN ('Berlin', 'London');
+
+-- Q4 :
+SELECT 
+    c.CustomerID,
+    ISNULL(p.FirstName + ' ' + p.LastName, s.Name) AS CustomerName,
+    a.City,
+    sp.Name AS StateProvince,
+    cr.Name AS CountryRegion
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+JOIN Person.BusinessEntityAddress bea ON c.CustomerID = bea.BusinessEntityID
+JOIN Person.Address a ON bea.AddressID = a.AddressID
+JOIN Person.StateProvince sp ON a.StateProvinceID = sp.StateProvinceID
+JOIN Person.CountryRegion cr ON sp.CountryRegionCode = cr.CountryRegionCode
+WHERE cr.Name IN ('United Kingdom', 'United States');
+
+
+-- Q5 :
+SELECT 
+    ProductID,
+    Name AS ProductName,
+    ProductNumber,
+    Color,
+    StandardCost,
+    ListPrice
+FROM Production.Product
+ORDER BY [Name];
+
+-- Q6 :
+SELECT ProductID, Name AS ProductName
+FROM Production.Product
+WHERE Name LIKE 'A%';
+
+-- Q7 :
+SELECT DISTINCT 
+    c.CustomerID,
+    COALESCE(p.FirstName + ' ' + p.LastName, s.Name) AS CustomerName
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+ORDER BY CustomerName;
+
+-- Q8 :
+SELECT DISTINCT
+    c.CustomerID,
+    COALESCE(p.FirstName + ' ' + p.LastName, s.Name) AS CustomerName,
+    a.City
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+JOIN Person.BusinessEntityAddress bea ON 
+    COALESCE(p.BusinessEntityID, s.BusinessEntityID) = bea.BusinessEntityID
+JOIN Person.Address a ON bea.AddressID = a.AddressID
+JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product pr ON sod.ProductID = pr.ProductID
+WHERE a.City = 'London'
+  AND pr.Name = 'Chai'
+ORDER BY CustomerName;
+
+SELECT ProductID, Name FROM Production.Product WHERE Name LIKE '%Chai%'; -- 'chai' does not exist in the datset
+
+
+-- Q9 :
+SELECT 
+    c.CustomerID,
+    COALESCE(p.FirstName + ' ' + p.LastName, s.Name) AS CustomerName
+FROM Sales.Customer c
+LEFT JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+WHERE soh.SalesOrderID IS NULL
+ORDER BY CustomerName;
+
+
+-- Q10 :
+SELECT DISTINCT
+    c.CustomerID,
+    COALESCE(p.FirstName + ' ' + p.LastName, s.Name) AS CustomerName
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product pr ON sod.ProductID = pr.ProductID
+WHERE pr.Name = 'Tofu'
+ORDER BY CustomerName;
+
+SELECT ProductID, Name FROM Production.Product WHERE Name LIKE '%Tofu%';  -- 'Tofu' does not exist
+
+
+
+
+
+
+
+
+
+
